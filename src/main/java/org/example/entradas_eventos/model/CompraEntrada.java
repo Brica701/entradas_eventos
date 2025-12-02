9af00c7679ca95f58061f4deb21551c6dabf4b8c
Brica701 <isaacruz2003@gmail.com>
2025-12-02 18:45:55 +0100

    sinVistaAdmin_algoDeValidacionesAñadidas

--- src/main/java/org/example/entradas_eventos/model/CompraEntrada.java
+++ src/main/java/org/example/entradas_eventos/model/CompraEntrada.java
@@ -2,6 +2,10 @@ package org.example.entradas_eventos.model;
 
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -17,11 +21,22 @@ import java.time.LocalDateTime;
 public class CompraEntrada {
 
     private Integer id;
+
+    @NotNull(message = "Debe seleccionar un evento")
     private Integer eventoId;
+
+    @NotNull(message = "Debe indicar el número de entradas")
+    @Min(value = 1, message = "Debe comprar al menos una entrada")
     private Integer numeroEntradas;
+
+    @NotBlank(message = "Debe seleccionar una zona")
     private String zona;
 
+    @NotBlank(message = "Debe ingresar su nombre")
     private String nombreComprador;
+
+    @Email(message = "Debe ingresar un email válido")
+    @NotBlank(message = "Debe ingresar su email")
     private String emailComprador;
 
     private BigDecimal precioUnitario;
